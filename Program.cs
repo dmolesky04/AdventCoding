a203b433a085a2a6e2940351be1c47ef1ec781a5
dave molesky <dmolesky04@gmail.com>
2022-12-01 14:56:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿var calorieData = File.ReadAllText("input.txt").Trim()
+    .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split("\n"))
+    .Select(x => x.Sum(n => int.Parse(n)))
+    .OrderByDescending(x => x);
+Console.WriteLine(calorieData.First());
+Console.WriteLine(calorieData.Take(3).Sum());
+
